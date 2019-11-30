b1460941bcd0b31a0f1e2be1c5313575ae235859
narendrakumar.d <desamnarendrakumar@gmail.com>
2019-11-30 14:30:47 +0530

    Staff Attendance layout

--- app/src/main/java/com/example/twdinspection/viewmodel/MPINCustomViewModel.java
+++ app/src/main/java/com/example/twdinspection/viewmodel/MPINCustomViewModel.java
@@ -1,5 +1,7 @@
 package com.example.twdinspection.viewmodel;
 
+import android.content.Context;
+
 import androidx.annotation.NonNull;
 import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProvider;
@@ -9,14 +11,16 @@ import com.example.twdinspection.databinding.ActivityMpinBinding;
 
 public class MPINCustomViewModel implements ViewModelProvider.Factory {
     private ActivityMpinBinding binding;
+    private Context context;
 
-    public MPINCustomViewModel(ActivityMpinBinding binding) {
+    public MPINCustomViewModel(ActivityMpinBinding binding, Context context) {
         this.binding = binding;
+        this.context = context;
     }
 
     @NonNull
     @Override
     public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
-        return (T) new MPINViewModel(binding);
+        return (T) new MPINViewModel(binding, context);
     }
 }
--- app/src/main/java/com/example/twdinspection/viewmodel/MPINViewModel.java
+++ app/src/main/java/com/example/twdinspection/viewmodel/MPINViewModel.java
@@ -1,5 +1,7 @@
 package com.example.twdinspection.viewmodel;
 
+import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
 import android.text.TextUtils;
 import android.view.View;
@@ -8,29 +10,33 @@ import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import com.example.twdinspection.databinding.ActivityMpinBinding;
+import com.example.twdinspection.ui.BasicDetailsActivity;
 import com.google.android.material.snackbar.Snackbar;
 
 public class MPINViewModel extends ViewModel {
 
     private ActivityMpinBinding binding;
+    private Context context;
 
     public MutableLiveData<String> mpin = new MutableLiveData<>();
 
-    MPINViewModel(ActivityMpinBinding binding) {
+    MPINViewModel(ActivityMpinBinding binding, Context context) {
         this.binding = binding;
+        this.context = context;
     }
 
     public void onVerifyClick() {
         if (TextUtils.isEmpty(mpin.getValue())) {
             showError();
-        }
-        else if (mpin.getValue().length()!=4) {
+        } else if (mpin.getValue().length() != 4) {
             showError();
         }
 
+        context.startActivity(new Intent(context, BasicDetailsActivity.class));
     }
 
-    private void showError(){
+    private void showError() {
+
         binding.pinview.setEnabled(false);
         Snackbar snackbar = Snackbar
                 .make(binding.pinview, "MPIN Required", Snackbar.LENGTH_INDEFINITE)
