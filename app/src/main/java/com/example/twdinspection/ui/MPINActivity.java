8f8690d0d33794e9c68f9b864ed0fc58a005de14
Niharika <nihapagudala@gmail.com>
2019-11-30 17:49:20 +0530

    Medical UI changes

--- app/src/main/java/com/example/twdinspection/ui/MPINActivity.java
+++ app/src/main/java/com/example/twdinspection/ui/MPINActivity.java
@@ -1,6 +1,7 @@
 package com.example.twdinspection.ui;
 
 import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.ViewModelProviders;
@@ -11,13 +12,13 @@ import com.example.twdinspection.databinding.ActivityMpinBinding;
 import com.example.twdinspection.viewmodel.MPINCustomViewModel;
 import com.example.twdinspection.viewmodel.MPINViewModel;
 
-public class MPINActivity extends AppCompatActivity{
+public class MPINActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ActivityMpinBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_mpin);
-        MPINViewModel mpinViewModel = ViewModelProviders.of(this, new MPINCustomViewModel(binding)).get(MPINViewModel.class);
+        MPINViewModel mpinViewModel = ViewModelProviders.of(this, new MPINCustomViewModel(binding, this)).get(MPINViewModel.class);
         binding.setViewmodel(mpinViewModel);
     }
 
