bb9ffef3aac148131832ad53e14cc52e653b9506
narendrakumar.d <desamnarendrakumar@gmail.com>
2019-12-02 15:55:11 +0530

    Permissions

--- app/src/main/java/com/example/twdinspection/utils/AppConstants.java
+++ app/src/main/java/com/example/twdinspection/utils/AppConstants.java
@@ -2,4 +2,10 @@ package com.example.twdinspection.utils;
 
 public class AppConstants {
     public static final String SHARED_PREF = "TWD_PREFERENCES";
+
+    public static final String SUCCESS = "SUCCESS";
+    public static final String FAILURE = "FAILURE";
+    public static final String WARNING = "WARNING";
+    public static final String INFO = "INFO";
+
 }
