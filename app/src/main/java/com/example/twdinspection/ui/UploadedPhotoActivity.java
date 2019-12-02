a6b758dad052eb3483fa876e16833bc5c07ab0ef
Niharika <nihapagudala@gmail.com>
2019-12-02 15:13:05 +0530

    Uploaded Photos UI changes

--- app/src/main/java/com/example/twdinspection/ui/UploadedPhotoActivity.java
+++ app/src/main/java/com/example/twdinspection/ui/UploadedPhotoActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.twdinspection.R;
 import com.example.twdinspection.databinding.ActivityGenerateMpinBinding;
@@ -15,6 +16,7 @@ import com.example.twdinspection.databinding.ActivityUploadedPhotoBinding;
 public class UploadedPhotoActivity extends AppCompatActivity {
 
     ActivityUploadedPhotoBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,10 +24,10 @@ public class UploadedPhotoActivity extends AppCompatActivity {
         TextView tv_title = findViewById(R.id.header_title);
         tv_title.setText("Uploaded Photographs");
 
-        binding.btnLayout.btnNext.setOnClickListener(new View.OnClickListener() {
+        binding.btnSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(UploadedPhotoActivity.this, UploadedPhotoActivity.class));
+                Toast.makeText(UploadedPhotoActivity.this, "Completed", Toast.LENGTH_SHORT).show();
             }
         });
     }
