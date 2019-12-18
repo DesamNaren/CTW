a70c77903991bcfa8fcda3cf3356939dd89bd981
sindhu.k <sindhu.k@cgg.gov.in>
2019-12-18 13:21:39 +0530

    StudAttendance functionality commit

--- app/src/main/java/com/example/twdinspection/adapter/StudentsAttAdapter.java
+++ app/src/main/java/com/example/twdinspection/adapter/StudentsAttAdapter.java
@@ -2,11 +2,14 @@ package com.example.twdinspection.adapter;
 
 
 import android.content.Context;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.RadioGroup;
 
 import androidx.annotation.NonNull;
 import androidx.databinding.DataBindingUtil;
@@ -46,7 +49,8 @@ public class StudentsAttAdapter extends RecyclerView.Adapter<StudentsAttAdapter.
         final StudAttendInfoEntity dataModel = list.get(position);
         holder.listItemBinding.setStudentAttend(dataModel);
 
-        holder.listItemBinding.tvClass.setText("Class " + (position + 1));
+//        holder.listItemBinding.tvClass.setText("Class " + (position + 1));
+        holder.listItemBinding.tvClass.setText(list.get(i).getClass_type());
         holder.listItemBinding.getRoot().findViewById(R.id.class_header).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -78,6 +82,74 @@ public class StudentsAttAdapter extends RecyclerView.Adapter<StudentsAttAdapter.
             holder.listItemBinding.tvExpand.setBackground(context.getResources().getDrawable(R.drawable.downarrow_16));
             holder.listItemBinding.llEntries.setVisibility(View.GONE);
         }
+
+        holder.listItemBinding.rgIsAttndMarked12.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                if (radioGroup.getCheckedRadioButtonId() == R.id.rb_yes) {
+                    holder.listItemBinding.llStudPres.setVisibility(View.VISIBLE);
+                } else if (radioGroup.getCheckedRadioButtonId() == R.id.rb_no) {
+                    holder.listItemBinding.llStudPres.setVisibility(View.GONE);
+                    holder.listItemBinding.etStudMarkedPres.setText("");
+                    holder.listItemBinding.variance.setText("");
+                }
+            }
+        });
+
+        holder.listItemBinding.etStudPresInsp.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (holder.listItemBinding.etStudMarkedPres.getVisibility() == View.VISIBLE) {
+                    if(!holder.listItemBinding.etStudMarkedPres.getText().toString().isEmpty()&&
+                            !holder.listItemBinding.etStudPresInsp.getText().toString().isEmpty()){
+                        String var=String.valueOf(Integer.parseInt(holder.listItemBinding.etStudMarkedPres.getText().toString().trim())-
+                                Integer.parseInt(holder.listItemBinding.etStudPresInsp.getText().toString().trim()));
+                       holder.listItemBinding.variance.setText(var);
+                    }else {
+                        holder.listItemBinding.variance.setText("");
+                    }
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        holder.listItemBinding.etStudMarkedPres.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (holder.listItemBinding.etStudMarkedPres.getVisibility() == View.VISIBLE) {
+                    if (holder.listItemBinding.etStudMarkedPres.getVisibility() == View.VISIBLE) {
+                        if (!holder.listItemBinding.etStudMarkedPres.getText().toString().isEmpty() &&
+                                !holder.listItemBinding.etStudPresInsp.getText().toString().isEmpty()) {
+                            String var = String.valueOf(Integer.parseInt(holder.listItemBinding.etStudMarkedPres.getText().toString().trim()) -
+                                    Integer.parseInt(holder.listItemBinding.etStudPresInsp.getText().toString().trim()));
+                            holder.listItemBinding.variance.setText(var);
+                        } else {
+                            holder.listItemBinding.variance.setText("");
+                        }
+                    }
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
         holder.bind(dataModel);
     }
 
