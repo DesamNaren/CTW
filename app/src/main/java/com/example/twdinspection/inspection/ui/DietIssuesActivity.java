82db50e20003cb658d452aee2306315a6ebb3c29
Niharika <nihapagudala@gmail.com>
2019-12-27 17:28:44 +0530

    New Changes in DietIssues Activity

--- app/src/main/java/com/example/twdinspection/inspection/ui/DietIssuesActivity.java
+++ app/src/main/java/com/example/twdinspection/inspection/ui/DietIssuesActivity.java
@@ -10,6 +10,7 @@ import android.widget.TextView;
 
 import com.example.twdinspection.R;
 import com.example.twdinspection.databinding.ActivityDietIssuesBinding;
+import com.example.twdinspection.databinding.ActivityDietIssuesTempBinding;
 
 public class DietIssuesActivity extends AppCompatActivity {
 
@@ -18,7 +19,7 @@ public class DietIssuesActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ActivityDietIssuesBinding binding=DataBindingUtil.setContentView(this,R.layout.activity_diet_issues);
+        ActivityDietIssuesTempBinding binding=DataBindingUtil.setContentView(this,R.layout.activity_diet_issues_temp);
         TextView tv_title=findViewById(R.id.header_title);
         tv_title.setText("Diet Issues");
         binding.btnLayout.btnNext.setOnClickListener(new View.OnClickListener() {
