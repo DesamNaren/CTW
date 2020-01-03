9eb450f27aeba4ed32c30101b587a74e28b2a488
sindhu.k <sindhu.k@cgg.gov.in>
2020-01-03 14:59:51 +0530

    BenDetails commit

--- app/src/main/java/com/example/twdinspection/schemes/ui/BenDetailsActivity.java
+++ app/src/main/java/com/example/twdinspection/schemes/ui/BenDetailsActivity.java
@@ -8,14 +8,14 @@ import android.widget.TextView;
 
 import com.example.twdinspection.R;
 import com.example.twdinspection.databinding.ActivityBenDetailsActivtyBinding;
+import com.example.twdinspection.inspection.ui.BaseActivity;
 
-public class BenDetailsActivty extends AppCompatActivity {
+public class BenDetailsActivity extends BaseActivity {
 
     ActivityBenDetailsActivtyBinding benDetailsBinding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        benDetailsBinding = DataBindingUtil.setContentView(this, R.layout.activity_ben_details_activty);
-        benDetailsBinding.header.headerTitle.setText(getString(R.string.ben_details));
+        benDetailsBinding = putContentView(R.layout.activity_ben_details_activty, getString(R.string.ben_report));
     }
 }
