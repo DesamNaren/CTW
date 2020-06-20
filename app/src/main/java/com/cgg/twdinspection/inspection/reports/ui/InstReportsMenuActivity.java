a1daf33418e01ba490b796bcd70081ff6cf432a6
Niharika <nihapagudala@gmail.com>
2020-06-20 13:14:26 +0530

    school general info pdf changes

--- app/src/main/java/com/cgg/twdinspection/inspection/reports/ui/InstReportsMenuActivity.java
+++ app/src/main/java/com/cgg/twdinspection/inspection/reports/ui/InstReportsMenuActivity.java
@@ -1,26 +1,42 @@
 package com.cgg.twdinspection.inspection.reports.ui;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.databinding.DataBindingUtil;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.cgg.twdinspection.R;
+import com.cgg.twdinspection.common.application.TWDApplication;
+import com.cgg.twdinspection.common.screenshot.PDFUtil;
+import com.cgg.twdinspection.common.utils.AppConstants;
 import com.cgg.twdinspection.common.utils.CustomProgressDialog;
+import com.cgg.twdinspection.common.utils.Utils;
+import com.cgg.twdinspection.databinding.ReportsInstMenuActivityBinding;
 import com.cgg.twdinspection.inspection.reports.adapter.ReportsMenuSectionsAdapter;
+import com.cgg.twdinspection.inspection.reports.source.InspReportData;
 import com.cgg.twdinspection.inspection.ui.DashboardActivity;
 import com.cgg.twdinspection.inspection.ui.LocBaseActivity;
-import com.cgg.twdinspection.databinding.ReportsInstMenuActivityBinding;
+import com.google.gson.Gson;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 
-public class InstReportsMenuActivity extends LocBaseActivity {
+public class InstReportsMenuActivity extends LocBaseActivity implements PDFUtil.PDFUtilListener {
+
     ReportsInstMenuActivityBinding binding;
     CustomProgressDialog customProgressDialog;
+    String directory_path, filePath;
+    SharedPreferences sharedPreferences;
+    private InspReportData reportData;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,7 +44,27 @@ public class InstReportsMenuActivity extends LocBaseActivity {
         binding = DataBindingUtil.setContentView(this, R.layout.reports_inst_menu_activity);
         binding.actionBar.headerTitle.setText(getString(R.string.insp_reports));
         binding.actionBar.ivPdf.setVisibility(View.VISIBLE);
+        
         customProgressDialog=new CustomProgressDialog(this);
+
+        sharedPreferences = TWDApplication.get(this).getPreferences();
+        Gson gson = new Gson();
+        String data = sharedPreferences.getString(AppConstants.INSP_REP_DATA, "");
+        reportData = gson.fromJson(data, InspReportData.class);
+
+        String jsonObject  = gson.toJson(reportData.getGeneralInfo());
+        if(!TextUtils.isEmpty(jsonObject) && !jsonObject.equalsIgnoreCase("{}")) {
+            binding.setInspData(reportData.getGeneralInfo());
+            binding.manNameTv.setText(reportData.getMandalName()+" & "+reportData.getVillageName());
+            binding.disNameTv.setText(reportData.getDistName());
+            binding.instNameTv.setText(reportData.getInstituteName());
+
+//            binding.includeBasicLayout.inspectionTime.setText(reportData.getGeneralInfo().getInspectionTime());
+//            binding.includeBasicLayout.offNme.setText(sharedPreferences.getString(AppConstants.OFFICER_ID, ""));
+//            binding.includeBasicLayout.offDes.setText(sharedPreferences.getString(AppConstants.OFFICER_DES, ""));
+            binding.executePendingBindings();
+        }
+
         binding.actionBar.backBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -46,12 +82,40 @@ public class InstReportsMenuActivity extends LocBaseActivity {
         });
         binding.actionBar.ivPdf.setOnClickListener(new View.OnClickListener() {
             @Override
+            public void onClick(View view) {
+                try {
+                    customProgressDialog.show();
+
+                    directory_path = getExternalFilesDir(null)
+                            + "/" + "TWD/Schemes/";
+
+                    filePath = directory_path + "School Inspection_" +
+                            sharedPreferences.getString(AppConstants.OFFICER_ID, "")
+                            + "_" + reportData.getGeneralInfo().getInspectionTime() + ".pdf";
+                    File file = new File(filePath);
+                    List<View> views = new ArrayList<>();
+                    views.add(binding.scrlPdf);
+
+                    PDFUtil.getInstance().generatePDF(views, filePath, InstReportsMenuActivity.this);
+
+                } catch (Exception e) {
+                    if (customProgressDialog.isShowing())
+                        customProgressDialog.hide();
+
+                    Toast.makeText(InstReportsMenuActivity.this, getString(R.string.something), Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+       /* binding.actionBar.ivPdf.setOnClickListener(new View.OnClickListener() {
+            @Override
             public void onClick(View v) {
                 customProgressDialog.show();
                 startActivity(new Intent(InstReportsMenuActivity.this, PreviewPdfActivity.class)
                         .setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK));
             }
-        });
+        });*/
 
         String[] stringArray = getResources().getStringArray(R.array.inst_sections);
         ArrayList<String> sections = new ArrayList<>(Arrays.asList(stringArray));
@@ -75,6 +139,20 @@ public class InstReportsMenuActivity extends LocBaseActivity {
         startActivity(new Intent(InstReportsMenuActivity.this, InspectionReportsDashboard.class)
                 .setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK));
         ;
+    }
+
+    @Override
+    public void pdfGenerationSuccess(File savedPDFFile) {
+        customProgressDialog.hide();
+
+        Utils.customSyncSuccessAlert(InstReportsMenuActivity.this, getString(R.string.app_name), "PDF saved successfully at " + savedPDFFile.getPath().toString());
+    }
+
+    @Override
+    public void pdfGenerationFailure(Exception exception) {
+        customProgressDialog.hide();
+
+        Utils.customErrorAlert(InstReportsMenuActivity.this, getString(R.string.app_name), getString(R.string.something) + " " + exception.getMessage());
     }
 }
 
