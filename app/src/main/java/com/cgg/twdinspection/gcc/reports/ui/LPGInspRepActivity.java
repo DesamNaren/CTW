ad02852e4ec606cb03bceb724673ed5e60bac6ab
Niharika <nihapagudala@gmail.com>
2020-07-07 13:59:37 +0530

    Punit and lpg pdf changes

--- app/src/main/java/com/cgg/twdinspection/gcc/reports/ui/LPGInspRepActivity.java
+++ app/src/main/java/com/cgg/twdinspection/gcc/reports/ui/LPGInspRepActivity.java
@@ -16,17 +16,25 @@ import com.bumptech.glide.request.RequestListener;
 import com.bumptech.glide.request.target.Target;
 import com.cgg.twdinspection.R;
 import com.cgg.twdinspection.common.application.TWDApplication;
+import com.cgg.twdinspection.common.screenshot.PDFUtil;
 import com.cgg.twdinspection.common.utils.AppConstants;
+import com.cgg.twdinspection.common.utils.CustomProgressDialog;
 import com.cgg.twdinspection.common.utils.Utils;
 import com.cgg.twdinspection.databinding.ActivityLpgInspRepBinding;
 import com.cgg.twdinspection.gcc.reports.source.ReportData;
 import com.google.gson.Gson;
 
-public class LPGInspRepActivity extends AppCompatActivity {
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LPGInspRepActivity extends AppCompatActivity implements PDFUtil.PDFUtilListener {
 
     ActivityLpgInspRepBinding binding;
     private SharedPreferences sharedPreferences;
     ReportData reportData;
+    CustomProgressDialog customProgressDialog;
+    String directory_path, filePath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +43,9 @@ public class LPGInspRepActivity extends AppCompatActivity {
 
         binding.bottomLl.btnNext.setText("Next");
         binding.header.headerTitle.setText("LPG FINDINGS REPPORT");
+        binding.header.ivPdf.setVisibility(View.VISIBLE);
+        customProgressDialog = new CustomProgressDialog(this);
+
         binding.header.backBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -53,6 +64,17 @@ public class LPGInspRepActivity extends AppCompatActivity {
         Gson gson = new Gson();
         String data = sharedPreferences.getString(AppConstants.REP_DATA, "");
         reportData = gson.fromJson(data, ReportData.class);
+        try {
+            if (reportData != null) {
+                binding.divName.setText(reportData.getDivisionName());
+                binding.socName.setText(reportData.getSocietyName());
+                binding.drGodownName.setText(reportData.getGodownName());
+                binding.inchargeName.setText(reportData.getInchargeName());
+                binding.tvDate.setText(reportData.getInspectionTime());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         if (reportData != null && reportData.getInspectionFindings() != null && reportData.getInspectionFindings().getLpg() != null) {
             binding.setInspData(reportData.getInspectionFindings().getLpg());
@@ -81,6 +103,11 @@ public class LPGInspRepActivity extends AppCompatActivity {
                                 }
                             })
                             .into(binding.ivRepairs);
+                    Glide.with(LPGInspRepActivity.this)
+                            .load(reportData.getPhotos().get(z).getFilePath())
+                            .error(R.drawable.no_image)
+                            .placeholder(R.drawable.camera)
+                            .into(binding.ivRepairsPdf);
                     break;
                 }
 
@@ -96,6 +123,33 @@ public class LPGInspRepActivity extends AppCompatActivity {
             Toast.makeText(this, getString(R.string.something), Toast.LENGTH_SHORT).show();
         }
 
+        binding.tvDate.setText(reportData.getInspectionTime());
+        binding.header.ivPdf.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    customProgressDialog.show();
+
+                    directory_path = getExternalFilesDir(null)
+                            + "/" + "TWD/GCC/";
+
+                    filePath = directory_path + "LPG_" + reportData.getOfficerId() + "_" + reportData.getInspectionTime() + ".pdf";
+                    File file = new File(filePath);
+                    List<View> views = new ArrayList<>();
+                    views.add(binding.titlePdf);
+                    views.add(binding.generalPdf);
+
+                    PDFUtil.getInstance().generatePDF(views, filePath, LPGInspRepActivity.this, "schemes");
+
+                } catch (Exception e) {
+                    if (customProgressDialog.isShowing())
+                        customProgressDialog.hide();
+
+                    Toast.makeText(LPGInspRepActivity.this, getString(R.string.something), Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
         binding.bottomLl.btnNext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -103,5 +157,19 @@ public class LPGInspRepActivity extends AppCompatActivity {
                         .putExtra(AppConstants.PHOTO_TITLE, "LPG PHOTOS"));
             }
         });
+    }
+
+    @Override
+    public void pdfGenerationSuccess(File savedPDFFile) {
+        customProgressDialog.hide();
+
+        Utils.customSyncSuccessAlert(LPGInspRepActivity.this, getString(R.string.app_name), "PDF saved successfully at " + savedPDFFile.getPath().toString());
+    }
+
+    @Override
+    public void pdfGenerationFailure(Exception exception) {
+        customProgressDialog.hide();
+
+        Utils.customErrorAlert(LPGInspRepActivity.this, getString(R.string.app_name), getString(R.string.something) + " " + exception.getMessage());
     }
 }
--- app/src/main/java/com/cgg/twdinspection/gcc/reports/ui/PUnitInspRepActivity.java
+++ app/src/main/java/com/cgg/twdinspection/gcc/reports/ui/PUnitInspRepActivity.java
@@ -11,16 +11,25 @@ import androidx.databinding.DataBindingUtil;
 
 import com.cgg.twdinspection.R;
 import com.cgg.twdinspection.common.application.TWDApplication;
+import com.cgg.twdinspection.common.screenshot.PDFUtil;
 import com.cgg.twdinspection.common.utils.AppConstants;
+import com.cgg.twdinspection.common.utils.CustomProgressDialog;
+import com.cgg.twdinspection.common.utils.Utils;
 import com.cgg.twdinspection.databinding.ActivityPunitInspRepBinding;
 import com.cgg.twdinspection.gcc.reports.source.ReportData;
 import com.google.gson.Gson;
 
-public class PUnitInspRepActivity extends AppCompatActivity {
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PUnitInspRepActivity extends AppCompatActivity implements PDFUtil.PDFUtilListener {
 
     ActivityPunitInspRepBinding binding;
     SharedPreferences sharedPreferences;
     ReportData reportData;
+    CustomProgressDialog customProgressDialog;
+    String directory_path, filePath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +38,8 @@ public class PUnitInspRepActivity extends AppCompatActivity {
 
         binding.bottomLl.btnNext.setText("Next");
         binding.header.headerTitle.setText("PROCESSING UNIT FINDINGS REPORT");
+        binding.header.ivPdf.setVisibility(View.VISIBLE);
+        customProgressDialog = new CustomProgressDialog(this);
         binding.header.backBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -47,6 +58,7 @@ public class PUnitInspRepActivity extends AppCompatActivity {
         Gson gson = new Gson();
         String data = sharedPreferences.getString(AppConstants.REP_DATA, "");
         reportData = gson.fromJson(data, ReportData.class);
+
         if (reportData != null && reportData.getInspectionFindings() != null && reportData.getInspectionFindings().getProcessingUnit() != null
                 && reportData.getInspectionFindings().getProcessingUnit().getRegisterBookCertificates() != null) {
             try {
@@ -66,6 +78,24 @@ public class PUnitInspRepActivity extends AppCompatActivity {
                 binding.remarksCashBank.etRemarks.setText(reportData.getInspectionFindings().getProcessingUnit().getRegisterBookCertificates().getCashBankBalRemarks());
                 binding.remarksVehLog.etRemarks.setText(reportData.getInspectionFindings().getProcessingUnit().getRegisterBookCertificates().getVehLogBookRemarks());
                 binding.remarks.etRemarks.setText(reportData.getInspectionFindings().getProcessingUnit().getGeneralFindings().getRemarks());
+
+                binding.remarksStockPdf.etRemarks.setText(reportData.getInspectionFindings().getProcessingUnit().getRegisterBookCertificates().getRawMatStockRegisterRemarks());
+                binding.remarksProcessingPdf.etRemarks.setText(reportData.getInspectionFindings().getProcessingUnit().getRegisterBookCertificates().getProcessingRegisterRemarks());
+                binding.remarksInwardPdf.etRemarks.setText(reportData.getInspectionFindings().getProcessingUnit().getRegisterBookCertificates().getInwardRegisterRemarks());
+                binding.remarksOutwardPdf.etRemarks.setText(reportData.getInspectionFindings().getProcessingUnit().getRegisterBookCertificates().getOutwardRegisterRemarks());
+                binding.remarksSaleInvPdf.etRemarks.setText(reportData.getInspectionFindings().getProcessingUnit().getRegisterBookCertificates().getSaleInvoiceBookRemarks());
+                binding.remarksLabAttPdf.etRemarks.setText(reportData.getInspectionFindings().getProcessingUnit().getRegisterBookCertificates().getLabourAttendRegisterRemarks());
+                binding.remarksFireNocPdf.etRemarks.setText(reportData.getInspectionFindings().getProcessingUnit().getRegisterBookCertificates().getFireDeptRemarks());
+                binding.remarksAmcPdf.etRemarks.setText(reportData.getInspectionFindings().getProcessingUnit().getRegisterBookCertificates().getAmcMachinaryRemarks());
+                binding.remarksAgmarkPdf.etRemarks.setText(reportData.getInspectionFindings().getProcessingUnit().getRegisterBookCertificates().getAgmarkCertRemarks());
+                binding.remarksFsaaiPdf.etRemarks.setText(reportData.getInspectionFindings().getProcessingUnit().getRegisterBookCertificates().getFsaaiCertRemarks());
+                binding.remarksEmptiesPdf.etRemarks.setText(reportData.getInspectionFindings().getProcessingUnit().getRegisterBookCertificates().getEmptiesRegisterRemarks());
+                binding.remarksBarrelsPdf.etRemarks.setText(reportData.getInspectionFindings().getProcessingUnit().getRegisterBookCertificates().getBarrelsAlumnCansRemarks());
+                binding.remarksCashBookPdf.etRemarks.setText(reportData.getInspectionFindings().getProcessingUnit().getRegisterBookCertificates().getCashBookRemarks());
+                binding.remarksCashBankPdf.etRemarks.setText(reportData.getInspectionFindings().getProcessingUnit().getRegisterBookCertificates().getCashBankBalRemarks());
+                binding.remarksVehLogPdf.etRemarks.setText(reportData.getInspectionFindings().getProcessingUnit().getRegisterBookCertificates().getVehLogBookRemarks());
+                binding.remarksPdf.etRemarks.setText(reportData.getInspectionFindings().getProcessingUnit().getGeneralFindings().getRemarks());
+
             } catch (Exception e) {
                 Toast.makeText(this, getString(R.string.something) + " " + e.getMessage(), Toast.LENGTH_SHORT).show();
             }
@@ -85,5 +115,47 @@ public class PUnitInspRepActivity extends AppCompatActivity {
                         .putExtra(AppConstants.PHOTO_TITLE, "PROCESSING UNIT PHOTOS"));
             }
         });
+        binding.tvDate.setText(reportData.getInspectionTime());
+        binding.header.ivPdf.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    customProgressDialog.show();
+
+                    directory_path = getExternalFilesDir(null)
+                            + "/" + "TWD/GCC/";
+
+                    filePath = directory_path + "Processing_Unit_" + reportData.getOfficerId() + "_" + reportData.getInspectionTime() + ".pdf";
+                    File file = new File(filePath);
+                    List<View> views = new ArrayList<>();
+                    views.add(binding.registersPdf);
+                    views.add(binding.generalPdf);
+
+                    PDFUtil.getInstance().generatePDF(views, filePath, PUnitInspRepActivity.this, "schemes");
+
+                } catch (Exception e) {
+                    if (customProgressDialog.isShowing())
+                        customProgressDialog.hide();
+
+                    Toast.makeText(PUnitInspRepActivity.this, getString(R.string.something), Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+    }
+
+    @Override
+    public void pdfGenerationSuccess(File savedPDFFile) {
+        customProgressDialog.hide();
+
+        Utils.customSyncSuccessAlert(PUnitInspRepActivity.this, getString(R.string.app_name), "PDF saved successfully at " + savedPDFFile.getPath().toString());
+    }
+
+    @Override
+    public void pdfGenerationFailure(Exception exception) {
+        customProgressDialog.hide();
+
+        Utils.customErrorAlert(PUnitInspRepActivity.this, getString(R.string.app_name), getString(R.string.something) + " " + exception.getMessage());
     }
 }
