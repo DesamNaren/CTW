226068394a4097c543eae3cc04c245e0f5490580
Niharika <nihapagudala@gmail.com>
2020-08-06 11:35:08 +0530

    eng works pdf and dr depot iage compression

--- app/src/main/java/com/cgg/twdinspection/engineering_works/reports/ui/EngReportDetailsActivity.java
+++ app/src/main/java/com/cgg/twdinspection/engineering_works/reports/ui/EngReportDetailsActivity.java
@@ -5,11 +5,13 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.Handler;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
+import androidx.recyclerview.widget.GridLayoutManager;
 
 import com.cgg.twdinspection.R;
 import com.cgg.twdinspection.common.application.TWDApplication;
@@ -19,6 +21,7 @@ import com.cgg.twdinspection.common.utils.CustomProgressDialog;
 import com.cgg.twdinspection.common.utils.Utils;
 import com.cgg.twdinspection.databinding.ActivityEngReportDetailsBinding;
 import com.cgg.twdinspection.engineering_works.reports.source.ReportWorkDetails;
+import com.cgg.twdinspection.gcc.reports.adapter.ViewPhotoAdapterPdf;
 import com.cgg.twdinspection.inspection.ui.DashboardActivity;
 import com.google.gson.Gson;
 
@@ -33,6 +36,8 @@ public class EngReportDetailsActivity extends AppCompatActivity implements PDFUt
     private ReportWorkDetails reportWorkDetails;
     CustomProgressDialog customProgressDialog;
     String directory_path, filePath;
+    Gson gson;
+    ViewPhotoAdapterPdf adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,6 +58,7 @@ public class EngReportDetailsActivity extends AppCompatActivity implements PDFUt
                 finish();
             }
         });
+
         binding.header.ivPdf.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -112,7 +118,7 @@ public class EngReportDetailsActivity extends AppCompatActivity implements PDFUt
 
         try {
             sharedPreferences = TWDApplication.get(EngReportDetailsActivity.this).getPreferences();
-            Gson gson = new Gson();
+            gson = new Gson();
             String data = sharedPreferences.getString(AppConstants.ENG_REPORT_DATA, "");
             reportWorkDetails = gson.fromJson(data, ReportWorkDetails.class);
             binding.setWorkDetails(reportWorkDetails);
@@ -122,82 +128,86 @@ public class EngReportDetailsActivity extends AppCompatActivity implements PDFUt
             Toast.makeText(this, getString(R.string.something), Toast.LENGTH_SHORT).show();
             e.printStackTrace();
         }
-        try {
-//            if (reportWorkDetails.getPhotos() != null && reportWorkDetails.getPhotos().size() > 0) {
+        String jsonObject = gson.toJson(reportWorkDetails.getPhotos());
+        if (!TextUtils.isEmpty(jsonObject) && !jsonObject.equalsIgnoreCase("[]")) {
+            adapter = new ViewPhotoAdapterPdf(this, reportWorkDetails.getPhotos());
+            binding.recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
+            binding.recyclerView.setAdapter(adapter);
+        }
+       /* try {
+            if (reportWorkDetails.getPhotos() != null && reportWorkDetails.getPhotos().size() > 0) {
 
-//                if (reportWorkDetails.getPhotos().size() >= 1 && reportWorkDetails.getPhotos().get(0) != null && reportWorkDetails.getPhotos().get(0) != null) {
-//
-//                    binding.pbar.setVisibility(View.VISIBLE);
-//
-//
-//                    Glide.with(EngReportDetailsActivity.this)
-//                            .load(reportWorkDetails.getPhotos().get(0).getFilePath())
-//                            .error(R.drawable.no_image)
-//                            .placeholder(R.drawable.camera)
-//                            .listener(new RequestListener<String, GlideDrawable>() {
-//                                @Override
-//                                public boolean onException(Exception e, String model, Target<GlideDrawable> target, boolean isFirstResource) {
-//                                    binding.pbar.setVisibility(View.GONE);
-//                                    return false;
-//                                }
-//
-//                                @Override
-//                                public boolean onResourceReady(GlideDrawable resource, String model, Target<GlideDrawable> target, boolean isFromMemoryCache, boolean isFirstResource) {
-//                                    binding.pbar.setVisibility(View.GONE);
-//                                    return false;
-//                                }
-//                            })
-//                            .into(binding.ivCam1);
-//
-//                    binding.ivCam1.setOnClickListener(new View.OnClickListener() {
-//                        @Override
-//                        public void onClick(View v) {
-//                            Utils.displayPhotoDialogBox(reportWorkDetails.getPhotos().get(0).getFilePath(), EngReportDetailsActivity.this, "", false);
-//                        }
-//                    });
-//                }
+                if (reportWorkDetails.getPhotos().size() >= 1 && reportWorkDetails.getPhotos().get(0) != null && reportWorkDetails.getPhotos().get(0) != null) {
 
-//                if (reportWorkDetails.getPhotos().size() >= 2 && reportWorkDetails.getPhotos().get(1) != null && reportWorkDetails.getPhotos().get(1) != null) {
-//
-//
-//                    binding.pbar2.setVisibility(View.VISIBLE);
-//
-//                    Glide.with(EngReportDetailsActivity.this)
-//                            .load(reportWorkDetails.getPhotos().get(1).getFilePath())
-//                            .error(R.drawable.no_image)
-//                            .placeholder(R.drawable.camera)
-//                            .listener(new RequestListener<String, GlideDrawable>() {
-//                                @Override
-//                                public boolean onException(Exception e, String model, Target<GlideDrawable> target, boolean isFirstResource) {
-//                                    binding.pbar2.setVisibility(View.GONE);
-//                                    return false;
-//                                }
-//
-//                                @Override
-//                                public boolean onResourceReady(GlideDrawable resource, String model, Target<GlideDrawable> target, boolean isFromMemoryCache, boolean isFirstResource) {
-//                                    binding.pbar2.setVisibility(View.GONE);
-//                                    return false;
-//                                }
-//                            })
-//                            .into(binding.ivCam2);
-//
-//                    binding.ivCam2.setOnClickListener(new View.OnClickListener() {
-//                        @Override
-//                        public void onClick(View v) {
-//                            Utils.displayPhotoDialogBox(reportWorkDetails.getPhotos().get(1).getFilePath(), EngReportDetailsActivity.this, "", false);
-//                        }
-//                    });
-//
-//                }
-//            } else {
-//                binding.llPhotos.setVisibility(View.GONE);
-//            }
+                    binding.pbar.setVisibility(View.VISIBLE);
 
 
+                    Glide.with(EngReportDetailsActivity.this)
+                            .load(reportWorkDetails.getPhotos().get(0).getFilePath())
+                            .error(R.drawable.no_image)
+                            .placeholder(R.drawable.camera)
+                            .listener(new RequestListener<String, GlideDrawable>() {
+                                @Override
+                                public boolean onException(Exception e, String model, Target<GlideDrawable> target, boolean isFirstResource) {
+                                    binding.pbar.setVisibility(View.GONE);
+                                    return false;
+                                }
+
+                                @Override
+                                public boolean onResourceReady(GlideDrawable resource, String model, Target<GlideDrawable> target, boolean isFromMemoryCache, boolean isFirstResource) {
+                                    binding.pbar.setVisibility(View.GONE);
+                                    return false;
+                                }
+                            })
+                            .into(binding.ivCam1);
+
+                    binding.ivCam1.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Utils.displayPhotoDialogBox(reportWorkDetails.getPhotos().get(0).getFilePath(), EngReportDetailsActivity.this, "", false);
+                        }
+                    });
+                }
+
+                if (reportWorkDetails.getPhotos().size() >= 2 && reportWorkDetails.getPhotos().get(1) != null && reportWorkDetails.getPhotos().get(1) != null) {
+
+
+                    binding.pbar2.setVisibility(View.VISIBLE);
+
+                    Glide.with(EngReportDetailsActivity.this)
+                            .load(reportWorkDetails.getPhotos().get(1).getFilePath())
+                            .error(R.drawable.no_image)
+                            .placeholder(R.drawable.camera)
+                            .listener(new RequestListener<String, GlideDrawable>() {
+                                @Override
+                                public boolean onException(Exception e, String model, Target<GlideDrawable> target, boolean isFirstResource) {
+                                    binding.pbar2.setVisibility(View.GONE);
+                                    return false;
+                                }
+
+                                @Override
+                                public boolean onResourceReady(GlideDrawable resource, String model, Target<GlideDrawable> target, boolean isFromMemoryCache, boolean isFirstResource) {
+                                    binding.pbar2.setVisibility(View.GONE);
+                                    return false;
+                                }
+                            })
+                            .into(binding.ivCam2);
+
+                    binding.ivCam2.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Utils.displayPhotoDialogBox(reportWorkDetails.getPhotos().get(1).getFilePath(), EngReportDetailsActivity.this, "", false);
+                        }
+                    });
+
+                }
+            } else {
+                binding.llPhotos.setVisibility(View.GONE);
+            }
         } catch (Exception e) {
             e.printStackTrace();
 
-        }
+        }*/
 
     }
 
