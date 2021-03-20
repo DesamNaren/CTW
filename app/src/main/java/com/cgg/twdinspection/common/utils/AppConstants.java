3c9c7e3e4ab6159ee72a6a82da7f27a19f7d889d
Niharika <nihapagudala@gmail.com>
2021-03-20 12:17:08 +0530

    Appconstants commit

--- app/src/main/java/com/cgg/twdinspection/common/utils/AppConstants.java
+++ app/src/main/java/com/cgg/twdinspection/common/utils/AppConstants.java
@@ -182,4 +182,5 @@ public class AppConstants {
     public static final String OFFLINE_LPG = "lpg_godown";
     public static final String MPIN = "MPIN";
     public static final String SAMPLE2 = "SAMPLE2";
+    public static final String SAMPLE4 = "SAMPLE4";
 }
