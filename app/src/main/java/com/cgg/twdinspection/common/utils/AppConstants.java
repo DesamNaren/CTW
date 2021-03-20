b1c9029d6dce06ca7585e7a96a942e3f2431dde4
narendrakumar.d <narendrakumar.d@cgg.gov.in>
2021-03-20 12:16:42 +0530

    Test

--- app/src/main/java/com/cgg/twdinspection/common/utils/AppConstants.java
+++ app/src/main/java/com/cgg/twdinspection/common/utils/AppConstants.java
@@ -181,6 +181,5 @@ public class AppConstants {
     public static final String OFFLINE_PETROL = "petrol_godown";
     public static final String OFFLINE_LPG = "lpg_godown";
     public static final String MPIN = "MPIN";
-    public static final String SAMPLE = "SAMPLE";
-    public static final String SAMPLE1 = "SAMPLE1";
+    public static final String SAMPLE3 = "SAMPLE3";
 }
